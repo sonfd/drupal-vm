7775cf1dbca5027e2a8dcd53efb5e560dbbafff4
Eric Volkernick <eric.volkernick@gmail.com>
2016-05-14 19:34:04 -0400

    Updated config.yml to use project and site vars

--- template/template.scripts/import-db.sh
+++ template/template.scripts/import-db.sh
@@ -2,9 +2,9 @@
 DB=$1
 SQL=$2
 
-if [ ! -z $DB ] && [ ! -z $SQL ] && [ -f /var/www/$SQL ]; then
-  mysqldump -u root -proot $DB > /var/www/db/provision-backup/$1.sql
-  echo "$1 backed up to db/provision-backup/$1.sql"
+if [ ! -z $DB ] && [ ! -z $SQL ] && [ -f /web/db/$SQL ]; then
+  mysqldump -u root -proot $DB > /web/db/provision-backup/$DB.sql
+  echo "Created backup of $DB to db/provision-backup/$DB.sql"
 
   mysql -u root -proot $DB < $SQL
   echo "$SQL imported to $DB"
