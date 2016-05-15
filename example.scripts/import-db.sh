9602e33e1fafb708235a31e3ba8a1368787da5cd
Eric Volkernick <eric.volkernick@gmail.com>
2016-05-14 20:32:24 -0400

    Added example scripts with import-db.sh

---
+++ example.scripts/import-db.sh
@@ -0,0 +1,15 @@
+# Backup db and import db
+DB=$1
+SQL=$2
+
+if [ ! -z $DB ] && [ ! -z $SQL ] && [ -f /var/www/$SQL ]; then
+  vagrant ssh
+  
+  mysqldump -u root -proot $DB > /var/www/db/provision-backup/$1.sql
+  echo "$1 backed up to db/provision-backup/$1.sql"
+
+  mysql -u root -proot $DB < $SQL
+  echo "$SQL imported to $DB"
+else
+  echo "No DB Imported"
+fi
