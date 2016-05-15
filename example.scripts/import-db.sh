68b7f7e8e7bdaa02accfdcab09ba064e9d72e187
Eric Volkernick <eric.volkernick@gmail.com>
2016-05-14 22:55:52 -0400

    Tidied up import script

--- example.scripts/import-db.sh
+++ example.scripts/import-db.sh
@@ -2,11 +2,11 @@
 DB=$1
 SQL=$2
 
-if [ ! -z $DB ] && [ ! -z $SQL ] && [ -f /var/www/$SQL ]; then
+if [ ! -z $DB ] && [ ! -z $SQL ] && [ -f /web/db/$SQL ]; then
   vagrant ssh
-  
-  mysqldump -u root -proot $DB > /var/www/db/provision-backup/$1.sql
-  echo "$1 backed up to db/provision-backup/$1.sql"
+
+  mysqldump -u root -proot $DB > /web/db/provision-backup/$DB.sql
+  echo "$DB backed up to db/provision-backup/$DB.sql"
 
   mysql -u root -proot $DB < $SQL
   echo "$SQL imported to $DB"
